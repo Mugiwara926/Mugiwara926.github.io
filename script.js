9ed65827a5fdd965ff393930e4932a8b455a8373
ilyas <falconilyas@gmail.com>
2026-04-08 21:48:48 +0300

    yetenekler ve eğitim yeri oluşturuldu interaktif yetenek barları oluşturuldu

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ window.addEventListener("mousemove", (e) => {
     fare.style.left = e.clientX + "px";
     fare.style.top = e.clientY + "px";
 });
+
+const barlar = document.querySelectorAll('.bar-ic');
+        const gozlemci = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    entry.target.style.width = entry.target.getAttribute('data-doluluk');
+                }
+            });
+        }, { threshold: 0.5 });
+
+    barlar.forEach(bar => gozlemci.observe(bar));
